160847d40b3013792b6228ef85080abd727cdbc7
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-03 19:17:12 +0100

    complete lab

--- optional_lab_array_hash_loop/start_point/my_functions.rb
+++ optional_lab_array_hash_loop/start_point/my_functions.rb
@@ -1,18 +1,18 @@
 
-#
-# def add_array_lengths(array1, array2)
-#   result = array1.length + array2.length
-#   return result
-# end
+
+def add_array_lengths(array1, array2)
+  result = array1.length + array2.length
+  return result
+end
 
 
-# def sum_array(numbers)
-#   count = 0
-#   for number in numbers
-#     count += number
-#   end
-#   return count
-# end
+def sum_array(numbers)
+  count = 0
+  for number in numbers
+    count += number
+  end
+  return count
+end
 
 
 def find_item(array, item)
@@ -20,7 +20,10 @@ def find_item(array, item)
     if house == item
       return true
     end
-
   end
   return false
+end
+
+def get_first_key(hash)
+  first_key = hash.keys[0]
 end
