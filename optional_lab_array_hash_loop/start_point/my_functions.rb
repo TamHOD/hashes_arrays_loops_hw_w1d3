77392e26c25292610108a6c19a602e245989571c
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-03 19:11:58 +0100

    lab exercise 3 completed

--- optional_lab_array_hash_loop/start_point/my_functions.rb
+++ optional_lab_array_hash_loop/start_point/my_functions.rb
@@ -6,10 +6,21 @@
 # end
 
 
-def sum_array(numbers)
-  count = 0
-  for number in numbers
-    count += number
+# def sum_array(numbers)
+#   count = 0
+#   for number in numbers
+#     count += number
+#   end
+#   return count
+# end
+
+
+def find_item(array, item)
+  for house in array
+    if house == item
+      return true
+    end
+
   end
-  return count
+  return false
 end
