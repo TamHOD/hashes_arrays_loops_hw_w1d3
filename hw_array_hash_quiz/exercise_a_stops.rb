3efdaff0142056abf91e9480de45d82c085c4537
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-03 15:27:22 +0100

    Complete 1 & 2

--- hw_array_hash_quiz/exercise_a_stops.rb
+++ hw_array_hash_quiz/exercise_a_stops.rb
@@ -1,7 +1,11 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
+stops.push("Edinburgh Waverly")
+
 # 2. Add `"Glasgow Queen St"` to the start of the array
+stops[0] = "Glasgow Queen St"
+p stops
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 # 4. Work out the index position of `"Linlithgow"`
 # 5. Remove `"Livingston"` from the array using its name
